6664bb7c1f4367f5a10ee90b1afd74aeecd423c0
david <sankeyr3@gmail.com>
2023-08-15 17:58:58 +0200

    genocidio abuelistico

--- controller/controller.php
+++ controller/controller.php
@@ -15,6 +15,7 @@ class MVCcontroller {
     public function getArticuloByBarcode($barcode = null){
         $barcode = '%'.$barcode.'%';
         $data = $this->MVCmodel->runQuerySQL("SELECT * FROM articulo WHERE ConsultaParcial LIKE :barcode;", [":barcode" => "$barcode"], true);
+        header('Content-Type: application/json; charset=utf-8');
         echo json_encode($data);
     }
 }
