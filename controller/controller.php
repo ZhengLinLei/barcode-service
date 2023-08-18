468d2b25dd7c37b9b0a8e2875fd5086a0edad450
ZhengLinLei <zheng9112003@gmail.com>
2023-08-18 09:41:50 +0200

    #3 Fixed with testing

--- controller/controller.php
+++ controller/controller.php
@@ -12,11 +12,14 @@ class MVCcontroller {
     }
     // ... Add custom methods
 
-    public function getArticuloByBarcode($barcode = null){
+    public function getProductByBarcode($barcode = null){
         $barcode = '%'.$barcode.'%';
-        $data = $this->MVCmodel->runQuerySQL("SELECT * FROM $config['table_name'] WHERE $config['column_name'] LIKE :barcode;", [":barcode" => "$barcode"], true);
-        header('Content-Type: application/json; charset=utf-8');
-        echo json_encode($data);
+        
+        return $this->MVCmodel->runQuerySQL("SELECT * FROM $config['table_name'] WHERE $config['column_name'] LIKE :barcode;", [":barcode" => "$barcode"], true);
+        
+    }
+    public function formateResponse($status, $text, $content) {
+        die('{"client": {"POST": '.json_encode($_POST).', "GET": '.json_encode($_GET).'}, "server": {"status": '.$status.', "response": "'.$text.'", "content": '.$content.'}}');
     }
     public function scanDir($dir) {
         $result = array();
